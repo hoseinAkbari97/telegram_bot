bd2f7bf3fead57073c45922098d222d34395cba2
Hossein <hossein16akbari@gmail.com>
2025-09-17 13:36:26 +0330

    #12-the initial standard structure

--- core/main.py
+++ core/main.py
@@ -1,1 +1,14 @@
+import os
 import telebot
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+API_TOKEN = os.environ.get("API_TOKEN")
+if API_TOKEN is None:
+    raise ValueError("API_TOKEN is not set in environment variables")
+
+bot = telebot.TeleBot(API_TOKEN)
+
+bot.infinity_polling()
