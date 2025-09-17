c7fb52d420a28d2851b451481a593946f69783a3
Hossein <hossein16akbari@gmail.com>
2025-09-17 15:25:44 +0330

    #13-successfully send welcome

--- core/main.py
+++ core/main.py
@@ -1,6 +1,7 @@
 import os
 import telebot
 from dotenv import load_dotenv
+import pprint
 
 
 load_dotenv()
@@ -10,5 +11,15 @@ if API_TOKEN is None:
     raise ValueError("API_TOKEN is not set in environment variables")
 
 bot = telebot.TeleBot(API_TOKEN)
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+    pprint.pprint(message)
+    print(message)
 
 bot.infinity_polling()
