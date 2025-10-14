60177498ecd3c319e302217c5debf5e79003b6dc
hoseinAkbari97 <hossein16akbari@gmail.com>
2025-10-14 12:48:34 +0330

    #35- sending the voice using test_voice is working successfully

--- core/main.py
+++ core/main.py
@@ -69,51 +69,9 @@ def reply_call(call):
             timeout=5
         )
 
-    
-# @bot.message_handler(func = lambda message: message.text == "about")
-# def send_about(message):
-#     bot.send_message(message.chat.id, """We are a very powerfull company""")
-
-# @bot.message_handler(func = lambda message: message.text == "help")
-# def send_help(message):
-#     bot.send_message(message.chat.id, """What is wrong my friend?""")
-    
-# Handles all sent documents and audio files
-# @bot.message_handler(content_types=['document', 'audio'])
-# def handle_docs_audio(message):
-#     if message.content_type == "document":
-#         print("It's a document")
-#     elif message.content_type == "audio":
-#         print("It's an audio")
-
-# # Handles all text messages that match the regular expression
-# @bot.message_handler(regexp="ali")
-# def handle_message(message):
-# 	print("hi")
-     
-# def check_hello(message):
-#     return message.text == "hello"
-
-# @bot.message_handler(func=check_hello)
-# def handle_hello_message(message):
-#     print("Triggered")
-
-# @bot.edited_message_handler(func=lambda message:True)
-# def handle_edited_message(message):
-#     print("edited message is received")
-
-# @bot.message_handler(commands=['setname'])
-# def setup_name(message):
-#     bot.send_message(message.chat.id, "Please send me your name.")
-#     bot.register_next_step_handler(message, callback=assign_first_name)
-
-# def assign_first_name(message, *args, **kwargs):
-#     first_name = message.text
-#     bot.send_message(message.chat.id, "What is your last name?")
-#     bot.register_next_step_handler(message, callback=assign_last_name, first_name=first_name)
-
-# def assign_last_name(message, first_name):
-#     last_name = message.text
-#     bot.send_message(message.chat.id, f"Your full name is {first_name} {last_name}.")
+@bot.message_handler(commands=['test_voice'])
+def send_voice_file(message):
+    voice_file = open("./docs/test/files/test.mp3", "rb")
+    bot.send_voice(message.chat.id, voice_file)
 
 bot.infinity_polling()
